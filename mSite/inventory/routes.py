9c8d18a04a8b15cb89a050d85c5d3dc8b25e8141
smallken99 <smallken@gmail.com>
2023-09-21 12:42:14 +0800

    8月份庫存更新

--- mSite/inventory/routes.py
+++ mSite/inventory/routes.py
@@ -21,7 +21,7 @@ def inventory_handle():
 def get_statistics():
     query = text('''
     select `year_month` as 'year_month' ,`item_description` as 'item_description',sum(quantity) as 'quantity'   from mSite.inventory_details
-    where `status`= '2'  and `year_month` in ('2023-05','2023-06','2023-07')
+    where `status`= '2'  and `year_month` in ('2023-06','2023-07','2023-08')
     group by `year_month`,`item_description`
     order by sum(quantity) desc
     ''')
@@ -39,14 +39,14 @@ def get_statistics():
     new_data = []
     for d in data:
         item_description = d['item_description']
-        value_2023_05 = d['2023-05'] if not pd.isna(d['2023-05']) else '0'
         value_2023_06 = d['2023-06'] if not pd.isna(d['2023-06']) else '0'
         value_2023_07 = d['2023-07'] if not pd.isna(d['2023-07']) else '0'
+        value_2023_08 = d['2023-08'] if not pd.isna(d['2023-08']) else '0'
 
         new_dict = {'item_description': item_description, 
-                    '2023-05': int(value_2023_05),
                     '2023-06': int(value_2023_06),
                     '2023-07': int(value_2023_07),
+                    '2023-08': int(value_2023_08),
                     }
         new_data.append(new_dict)
 
